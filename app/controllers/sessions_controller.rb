eab8888ec1d52c38efc4e4f59b25b23254b44560
joshneipp <joshneipp@gmail.com>
2015-10-08 19:31:12 -0400

    Unsuccessful login redirects to login path.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,16 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
-    a = Author.find_by(params[:email])
+    a = Author.find_by_email(params[:email])
     if a && a.authenticate(params[:password])
       session[:author_logged_in] = true
       redirect_to surveys_path
     else
-      redirect_to login
+      redirect_to login_path
     end
   end
 
   def destroy
-
+    session[:author_logged_in] = nil
   end
 end
