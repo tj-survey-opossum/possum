26e95569164126f185d0ef7cfbdfefd95304b9f4
joshneipp <joshneipp@gmail.com>
2015-10-08 20:25:22 -0400

    Authors can log in.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,13 +6,41 @@ class SessionsController < ApplicationController
     a = Author.find_by_email(params[:email])
     if a && a.authenticate(params[:password])
       session[:author_logged_in] = true
-      redirect_to surveys_path
+      redirect_to surveys_path, notice: "Logged in!"
     else
-      redirect_to login_path
+      redirect_to login_path, warning: "Login failed. Invalid email/password."
     end
   end
 
   def destroy
     session[:author_logged_in] = nil
+    redirect_to login_path
   end
 end
+
+
+
+
+
+
+
+
+
+
+#
+#
+#
+#
+#     a = Author.find_by_email(params[:email])
+#     if a && a.authenticate(params[:password])
+#       session[:author_logged_in] = true
+#       redirect_to surveys_path
+#     else
+#       redirect_to login_path
+#     end
+#   end
+#
+#   def destroy
+#     session[:author_logged_in] = nil
+#   end
+# end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+author = Author.create!(name: "author", email: "a@example.com", password: "pword")
