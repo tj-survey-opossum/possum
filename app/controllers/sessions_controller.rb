178c8573f8215388d89fdbd22365d0b5bc07cc77
joshneipp <joshneipp@gmail.com>
2015-10-08 21:06:16 -0400

    Logged in authors redirected to surveys.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,30 +17,3 @@ class SessionsController < ApplicationController
     redirect_to login_path
   end
 end
-
-
-
-
-
-
-
-
-
-
-#
-#
-#
-#
-#     a = Author.find_by_email(params[:email])
-#     if a && a.authenticate(params[:password])
-#       session[:author_logged_in] = true
-#       redirect_to surveys_path
-#     else
-#       redirect_to login_path
-#     end
-#   end
-#
-#   def destroy
-#     session[:author_logged_in] = nil
-#   end
-# end
