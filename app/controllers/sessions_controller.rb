9970bb38eb51fb2750ff90ba8cf68b0a7d9a59ed
joshneipp <joshneipp@gmail.com>
2015-10-09 07:34:18 -0400

    Display author logged in header.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,17 +1,12 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
   def create
-
     a = Author.find_by_email(params[:email])
     if a && a.authenticate(params[:password])
-      session[:author_logged_in] = true
-      redirect_to surveys_path, notice: "Logged in!"
-
-    author = Author.find_by(params[:email])
-    if author && author.authenticate(params[:password])
-      session[:author_id] = author.id
+      session[:author_id] = a.id
       redirect_to surveys_path, notice: "Logged in!"
     else
       redirect_to login_path, warning: "Login failed. Invalid email/password."
