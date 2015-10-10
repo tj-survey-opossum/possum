99f9f02a2f88840a984ee0b1c2e31bee1b29ffd1
joshneipp <joshneipp@gmail.com>
2015-10-10 13:20:07 -0400

    Comment out lots of tests.

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,19 +1,38 @@
 require 'test_helper'
 
-class SessionsControllerTest < ActionController::TestCase
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should get create" do
-    get :create
+class SessionsControllerTest < ActionDispatch::IntegrationTest
+  test "login and logout works" do
+    get authors_path
     assert_redirected_to login_path
-  end
-
-  test "should get destroy" do
-    get :destroy
+    follow_redirect!
+    assert_template "new"
+    post login_path, session: {email: "gibberish@example.com", password: "(&*GFYIB)"}
     assert_redirected_to login_path
+    follow_redirect!
+    assert_template "new"
+    post login_path, session: {email: 'cloud@example.com', password: 'aeris'}
+    assert_redirected_to surveys_path
   end
 
+
+  test "login" do
+    get login_path
+    assert_template "new"
+    post login_path, sessions: {email: "cloud@example.com", password: "aeris"}
+    assert_redirected_to surveys_path
+  end
 end
+# class LoginLogoutTest < ActionDispatch::IntegrationTest
+#   test "login page works" do
+#     get teachers_path
+#     assert_redirected_to login_path
+#     follow_redirect!
+#     assert_template "new"
+#     post login_path, sessions: {email: "gibberish@example.com", password: "(&*GFYIB)"}
+#     assert_redirected_to login_path
+#     post login_path, sessions: {email: "mason@example.com", password: "catsdrool"}
+#     assert_redirected_to teachers_path
+#     follow_redirect!
+#     assert_select "a[href=#{logout_path}]", 1
+#   end
+# end
