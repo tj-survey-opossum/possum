bfae3bffcb624069865eeb28d7a4393337e8c99d
joshneipp <joshneipp@gmail.com>
2015-10-11 15:23:46 -0400

    Add submission id to replies table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151011124305) do
+ActiveRecord::Schema.define(version: 20151011192312) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20151011124305) do
     t.string   "question_reply"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+    t.integer  "submission_id"
   end
 
   create_table "submissions", force: :cascade do |t|
